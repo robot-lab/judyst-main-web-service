950db4211b2b250a4ee103e301a6d5fca226c9b8
korwin <levozavr@mail.ru>
2018-10-21 22:26:19 +0300

    add start.sh as example

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo docker run --rm -p 5432:5432 -p 5672:5672 -p 5671:5671 -p 15672:15672 --name inf qio0/rmq_psql /bin/bash ./start.sh &
+sleep 2m
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py createsuperuser
+python3 manage.py runserver
