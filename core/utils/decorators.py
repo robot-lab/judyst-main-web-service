225c5a428998217213939a877fa5e667572b7f61
korwin <levozavr@mail.ru>
2018-10-21 22:20:39 +0300

    add documentation for utils

--- core/utils/decorators.py
+++ core/utils/decorators.py
@@ -3,6 +3,12 @@ from rest_framework.authentication import get_authorization_header
 
 
 def redirect_if_authorize(func):
+    """
+    decorator to check is user authorized and if it is so
+    it redirect user for another page
+    :param func: function for decorating
+    :return: decorating function
+    """
     def wrapper(self, request, *args, **kwargs):
         if 'Token' in get_authorization_header(request).decode():
             return redirect('/')
--- core/utils/functions.py
+++ core/utils/functions.py
@@ -3,12 +3,24 @@ from rest_framework.authtoken.models import Token
 
 
 def get_token(username, password):
+    """
+    function for generate token for user by email and password
+    :param username: user email
+    :param password: user password
+    :return: token
+    """
     session_user = authenticate(username=username, password=password)
     token, _ = Token.objects.get_or_create(user=session_user)
     return token
 
 
 def validate(data, fileds):
+    """
+    validator for checkin are these fields in data
+    :param data: data for validate
+    :param fileds: must have fields
+    :return: False if data is correct
+    """
     for filed in fileds:
         if data.get(filed) is None or data.get(filed) == "":
             return True
