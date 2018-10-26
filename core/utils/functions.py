7225ed4c859f1990cdc36bc6e13dd9281c1e6270
Rexarrior <Rexarrior@yandex.ru>
2018-10-26 18:02:51 +0300

    fix bag

--- core/utils/functions.py
+++ core/utils/functions.py
@@ -76,7 +76,7 @@ def is_not_valid_text_fields(data, fields, max_length=None, min_length=None,
     """
     for filed in fields:
         line = data.get(filed)
-        if line is None or line == "":
+        if not isinstane(line, str) or line == "":
             return True
         if max_length is not None and len(line) > max_length:
             return True
