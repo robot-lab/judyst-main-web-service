c547595e38b0d21c8c3c71bfc9e729aaf32aa0b3
Rexarrior <Rexarrior@yandex.ru>
2018-11-01 00:31:19 +0300

    Adding data_utils: methods for updating database from local file storage

--- celery/data.py
+++ celery/data.py
@@ -1,4 +1,4 @@
-
+# encoding: utf-8
 import django
 import os
 import sys
@@ -142,65 +142,4 @@ if __name__ == '__main__':
     # a.edit_data({"email": "lev@mail.ru"}, "CustomUser",
     #               username="korwin@mail.ru")
     # print(a.get_all_data("email", "CustomUser"))
-    a = ModelData()
-    import web_crawler
-    from web_crawler import DataType
-    import link_analysis
-    import time
-    # Coping local file base into database
-    # source = web_crawler.ksrf_models.LocalFileStorageSource()
-    # source.folder_path = './tmp/programming/Judyst/files'
-    # source.prepare()
-    # databaseSource = web_crawler.\
-    #                  ksrf_models.KSRFDatabaseWrapper('KSRFDatabase', a)
-    # web_crawler.tools.updatae_database_from_source(databaseSource, source)
-   
-    # checking documents in database
-    # print(time.time())
-    # databaseSource = web_crawler.\
-    #                  ksrf_models.KSRFDatabaseWrapper('KSRFDatabase', a)
-    # print(databaseSource.get_all_data(DataType.DOCUMENT_HEADER))
-    # print(databaseSource.get_data('КСРФ/2476-О/2018', DataType.DOCUMENT_TEXT))
-    # print(time.time())
-
-    # collecting links in Result folder
-    # print(time.time())
-    # source = web_crawler.\
-    #     ksrf_models.\
-    #     KSRFDatabaseWrapper('KSRFDatabase', a)
-    # link_analysis.Init(source)
-    # link_analysis.process_period(
-    #     firstDateOfDocsForProcessing='18.03.1900',
-    #     lastDateOfDocsForProcessing='14.08.2019',
-    #     docTypesForProcessing={'КСРФ/О', 'КСРФ/П'},
-    #     firstDateForNodes='18.03.2014', lastDateForNodes='14.08.2017',
-    #     nodesIndegreeRange=(0, 25), nodesOutdegreeRange=(0, 25),
-    #     nodesTypes={'КСРФ/О', 'КСРФ/П'},
-    #     includeIsolatedNodes=False,
-    #     firstDateFrom='18.03.2016', lastDateFrom='14.08.2016',
-    #     docTypesFrom={'КСРФ/О', 'КСРФ/П'},
-    #     firstDateTo='18.03.2015', lastDateTo='14.08.2015',
-    #     docTypesTo={'КСРФ/О', 'КСРФ/П'},
-    #     weightsRange=(1, 5),
-    #     showPicture=False, sendRequestToUpdatingHeadersInBaseFromSite=False,
-    #     takeHeadersFromLocalStorage=False)
-
-    # print(time.time())
-    
-    # print(time.time())
-
-    # put links in database
-    print(time.time())
-    source = web_crawler.\
-        ksrf_models.\
-        KSRFDatabaseWrapper('KSRFDatabase', a)
-    links = link_analysis.converters.load_json('Results/cleanLinks.json')
-    source.put_data_collection(links, DataType.LINK)
-    print(time.time())
-
-
-    #check links in database
-    # print(time.time())
-    # print(Links.objects.all())
-    # print(time.time())
-        
+    pass
